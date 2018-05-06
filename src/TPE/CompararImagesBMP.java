d8ed2f8e8adb7995bf0e49386b6f00c258f5e7b9
jisegura <segurajuanignacio@outlook.com>
2018-05-06 17:18:50 -0300

    add CompararImagenes add Programa en main

---
+++ src/TPE/CompararImagesBMP.java
@@ -0,0 +1,112 @@
+package TPE;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class CompararImagesBMP {
+
+    private Double[] getProbabilidad(BufferedImage imagen) {
+        Double[] prob = new Double[256];
+        int pixeles = imagen.getHeight() * imagen.getWidth();
+
+        for (int i = 0; i < prob.length; i++) {
+            prob[i] = 0d;
+        }
+
+        for (int i = 0; i < imagen.getWidth(); i++) {
+            for (int j = 0; j < imagen.getHeight(); j++) {
+                int rgb = imagen.getRGB(i, j);
+                int color = (new Color(rgb, true)).getGreen();
+                prob[color]++;
+            }
+        }
+
+        for (int i = 0; i < prob.length; i++) {
+            prob[i] /= pixeles;
+        }
+
+        return prob;
+    }
+
+    private Double getMedia(Double[] prob) {
+        Double media = 0d;
+        for (int i = 0; i < prob.length; i++) {
+            media += i*prob[i];
+        }
+        return media;
+    }
+
+    private Double getCovarianza(BufferedImage imagenA, BufferedImage imagenB) {
+        Double[] probA = this.getProbabilidad(imagenA);
+        Double[] probB = this.getProbabilidad(imagenB);
+        Double mediaA = this.getMedia(probA);
+        Double mediaB = this.getMedia(probB);
+        Double covarianza = 0d;
+        int pixeles = imagenA.getWidth() * imagenA.getHeight();
+
+        for (int i = 0; i < imagenA.getWidth(); i++) {
+            for (int j = 0; j < imagenA.getHeight(); j++) {
+                int rgbA = imagenA.getRGB(i, j);
+                int rgbB = imagenB.getRGB(i, j);
+                int colorA = (new Color(rgbA, true)).getGreen();
+                int colorB = (new Color(rgbB, true)).getGreen();
+                covarianza += (colorA - mediaA) * (colorB - mediaB);
+            }
+        }
+
+        return covarianza / pixeles;
+    }
+
+    /*private Double getCovarianza(BufferedImage imagenA, BufferedImage imagenB) {
+        Double covarianza = 0d;
+        Double mediaAB = 0d;
+        Double mediaA = 0d;
+        Double mediaB = 0d;
+        int pixeles = imagenA.getWidth() * imagenA.getHeight();
+
+        for (int i = 0; i < imagenA.getWidth(); i++) {
+            for (int j = 0; j < imagenA.getHeight(); j++) {
+                int rgbA = imagenA.getRGB(i, j);
+                int rgbB = imagenB.getRGB(i, j);
+                int colorA = (new Color(rgbA, true)).getGreen();
+                int colorB = (new Color(rgbB, true)).getGreen();
+                mediaAB += colorA*colorB;
+                mediaA += colorA;
+                mediaB += colorB;
+            }
+        }
+
+        mediaAB /= pixeles;
+        mediaA /= pixeles;
+        mediaB /= pixeles;
+
+        covarianza = mediaAB - mediaA * mediaB;
+
+        return covarianza;
+    }*/
+
+    private Double getDesvioEstandar(BufferedImage imagen) {
+        Double[] prob = this.getProbabilidad(imagen);
+        Double media = this.getMedia(prob);
+        Double desvio = 0d;
+        int pixeles = imagen.getWidth() * imagen.getHeight();
+
+        for (int i = 0; i < imagen.getWidth(); i++) {
+            for (int j = 0; j < imagen.getHeight(); j++) {
+                int rgb = imagen.getRGB(i, j);
+                int color = (new Color(rgb, true)).getGreen();
+                desvio += Math.pow(color - media, 2);
+            }
+        }
+
+        return Math.sqrt(desvio / pixeles);
+    }
+
+    public Double getCoeficienteCorrelacion(BufferedImage imagenA, BufferedImage imagenB) {
+
+        double divisor = this.getDesvioEstandar(imagenA) * this.getDesvioEstandar(imagenB);
+
+        return this.getCovarianza(imagenA, imagenB) / divisor;
+    }
+
+}
--- src/TPE/Main.java
+++ src/TPE/Main.java
@@ -1,8 +1,42 @@
 package TPE;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
+    static String WILLORIGINAL = "Will(Original)";
+    static String WILL1 = "Will_1";
+    static String WILL2 = "Will_2";
+    static String WILL3 = "Will_3";
+    static String WILL4 = "Will_4";
+    static String WILL5 = "Will_5";
+    static String WILL6 = "Will_6";
+    static String WILL7 = "Will_7";
+    static String DIR_RES = "/home/jis/Project/Teoria-de-la-Informacion/src/TPE/res";
+
     public static void main(String[] args) {
+        String[] willArr = new String[]{WILL1, WILL2, WILL3, WILL4, WILL5, WILL6, WILL7};
+        HashMap<Double, ArrayList<String>> solucion = new HashMap();
+        Double r;
+        CompararImagesBMP cimg = new CompararImagesBMP();
+
+        try {
+            BufferedImage imgOriginal = ImageIO.read(new File(DIR_RES+"/img/Will/"+WILLORIGINAL+".bmp"));
+            r = cimg.getCoeficienteCorrelacion(imgOriginal, imgOriginal);
+            System.out.println("imgOriginal: " + r);
+            for (String imgName : willArr) {
+                BufferedImage imgComparar = ImageIO.read(new File(DIR_RES+"/img/Will/"+imgName+".bmp"));
+                r = cimg.getCoeficienteCorrelacion(imgOriginal, imgComparar);
+                System.out.println(imgName + ": " + r);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
